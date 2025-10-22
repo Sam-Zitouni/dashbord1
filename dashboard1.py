8fa9e9334528c3fab5f7c02327436510f648bbcd
Sam-Zitouni <samarzitouni256@gmail.com>
2025-10-22 14:50:17 +0100

    ds

--- dashboard1.py
+++ dashboard1.py
@@ -64,7 +64,7 @@ def init_connection_pool():
             host = "37.187.33.50"
             port = 5432
             database = "postgres"
-            user = "readonly_user"
+            user = "postgres"
             password = "secure_password"
             
 
